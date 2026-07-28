d13ddfb8c8ab77b0c0ac8d2fa9556059ee986f93
owowowowoamndian <ytuseupload@gmail.com>
2026-07-28 13:00:32 +0530

    Add SDC-II

--- SDC-I/ai-summary-1/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true }, // <--- New Field
-  password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
